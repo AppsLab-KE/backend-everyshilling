d2d0552492252de01145ab442f6261ee2887937d
cosBett <cosmasbett9@gmail.com>
2023-03-17 06:26:08 +0300

    updated database settings

--- services/app-otp/otp/settings.py
+++ services/app-otp/otp/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'otp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'remote-server-ip-address',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
