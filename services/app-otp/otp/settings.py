f947db293b1d06438bd360c6a13db29a43e67b76
cosBett <cosmasbett9@gmail.com>
2023-03-17 05:37:09 +0300

    added Taskfile

--- services/app-otp/otp/settings.py
+++ services/app-otp/otp/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'verification',
-    'djangorestframework'
-    ''
+    'djangorestframework',
+    'invoke',
 ]
 
 MIDDLEWARE = [
