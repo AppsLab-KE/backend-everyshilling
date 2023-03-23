2f5bc21b3dd51eb149e8882f2d657087612ae534
oyamo <oyamo.xyz@gmail.com>
2023-03-23 11:14:20 +0300

    setup database client

--- services/app-auth/internal/platform/database/grpc/app_db_client.go
+++ services/app-auth/internal/platform/database/grpc/app_db_client.go
@@ -13,15 +13,15 @@ func (c Client) Create(ctx context.Context, data entity.User) error {
 	return nil
 }
 
-func Update(ctx context.Context, user entity.User) error {
+func (c Client) Update(ctx context.Context, user entity.User) error {
 	return nil
 }
 
-func FindByID(ctx context.Context, uuid string) (*entity.User, error) {
+func (c Client) FindByID(ctx context.Context, uuid string) (*entity.User, error) {
 	panic("not implemented")
 }
 
-func FindByEmail(ctx context.Context, emailAddress string) (*entity.User, error) {
+func (c Client) FindByEmail(ctx context.Context, emailAddress string) (*entity.User, error) {
 	panic("not implemented")
 }
 
