73dfd381630fddb760507fce67368d17706a22e2
oyamo <oyamo.xyz@gmail.com>
2023-03-28 09:58:08 +0300

    add server

--- services/app-auth/internal/routes/server/server.go
+++ services/app-auth/internal/routes/server/server.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/usecase"
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/routes/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+const BaseUrl = "/api"
+
+type ServerConfig struct {
+	AuthUsecase usecase.AuthUseCase
+}
+
+func NewServer(cfg ServerConfig) *gin.Engine {
+	r := gin.Default()
+	options := handlers.GinServerOptions{
+		BaseURL: BaseUrl,
+	}
+
+	// Map handlers
+	h := handlers.NewHandler(cfg.AuthUsecase)
+	handlers.RegisterHandlersWithOptions(r, h, options)
+
+	return r
+}
