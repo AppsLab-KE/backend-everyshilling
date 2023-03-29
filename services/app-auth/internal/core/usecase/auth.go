cbb0b723f0a0cce48a87c8adff2002f68ec4d32e
oyamo <oyamo.xyz@gmail.com>
2023-03-29 23:28:24 +0300

    refactor: revamp login logic

--- services/app-auth/internal/core/usecase/auth.go
+++ services/app-auth/internal/core/usecase/auth.go
@@ -1,10 +1,23 @@
 package usecase
 
-import "github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/adapters"
+import (
+	"context"
+
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/adapters"
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/entity"
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/dto"
+)
 
 type AuthUseCase struct {
 	authService    adapters.AuthService
 	sessionService adapters.SessionService
+}
+
+func (a *AuthUseCase) RegisterUser(ctx context.Context, user *dto.RegisterJSONRequestBody) (*entity.User, error) {
+	// check if user exists
+	// if exists, return error
+	
+	return &entity.User{}, nil
 }
 
 func NewAuthUsecase(as adapters.AuthService, ss adapters.SessionService) *AuthUseCase {
--- services/app-auth/internal/routes/handlers/handler_wrapper.go
+++ services/app-auth/internal/routes/handlers/handler_wrapper.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/usecase"
+	"github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/dto"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,8 +16,21 @@ func (h Handler) PostLogin(c *gin.Context) {
 }
 
 func (h Handler) Register(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	// get request body
+	var requestBody dto.User
+	var responseBody dto.RegistrationResponse
+
+	// parse request body
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		responseBody.Email = nil;
+		responseBody.Name = nil;
+
+		c.JSON(400, responseBody)
+		return
+	}
+
+	// process the request
+	
 }
 
 func (h Handler) PostReset(c *gin.Context) {
