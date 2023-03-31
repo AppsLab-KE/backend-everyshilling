fe12898250c3c825fa6a07dee25c3bc0cf4fb4b8
MikeMwita <mwitamike8@gmail.com>
2023-03-31 09:31:57 +0300

    implemented resetpassword handler

--- services/app-auth/internal/core/usecase/auth.go
+++ services/app-auth/internal/core/usecase/auth.go
@@ -20,6 +20,15 @@ func (a *AuthUseCase) RegisterUser(ctx context.Context, user *dto.RegisterReques
 	return &entity.User{}, nil
 }
 
+func (a *AuthUseCase) ResetPassword(ctx context.Context, user *dto.RequestResetCredentials) (*entity.User, error) {
+	//check if the passwords match
+	//if they match ,return an error
+	//if RequestResetCredentials.Password == nil || RequestResetCredentials.ConfirmPassword == nil || *RequestResetCredentials.Password != *RequestResetCredentials.ConfirmPassword {
+	//	return errors.New("passwords do not match")
+
+	return &entity.User{}, nil
+}
+
 func NewAuthUsecase(as adapters.AuthService, ss adapters.SessionService) *AuthUseCase {
 	return &AuthUseCase{
 		authService:    as,
--- services/app-auth/internal/routes/handlers/handler_wrapper.go
+++ services/app-auth/internal/routes/handlers/handler_wrapper.go
@@ -53,8 +53,37 @@ func (h Handler) Register(c *gin.Context) {
 }
 
 func (h Handler) PostReset(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	//get the request body
+	var requestBody dto.RequestResetCredentials
+	var responseBody dto.DefaultRes
+
+	//parse the request body
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		responseBody.Message = "Password reset failed"
+		responseBody.Code = 400
+		responseBody.Data = nil
+		responseBody.Error = "Invalid request."
+		c.JSON(400, responseBody)
+		return
+	}
+	//process the request
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// check if the user exists and generate a reset request ID
+	err, _ := h.AuthUC.ResetPassword(ctx, &requestBody)
+	if err != nil {
+		responseBody.Message = "Password reset failed"
+		responseBody.Code = 400
+		responseBody.Data = nil
+		responseBody.Error = "Invalid request"
+		c.JSON(400, responseBody)
+		return
+	}
+	responseBody.Message = "Password reset successful"
+	responseBody.Code = 200
+	responseBody.Data = nil
+	responseBody.Error = ""
+	c.JSON(200, responseBody)
 }
 
 func (h Handler) PostResetRequestIdChange(c *gin.Context, requestId string) {
