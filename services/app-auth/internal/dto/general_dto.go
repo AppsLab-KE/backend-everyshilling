78d8e6d2e14d7b30216800b3ea55eccec585e14d
Marvin Collins <marvin.collins@scaratec.com>
2023-04-04 12:12:53 +0300

    WIP

--- services/app-auth/internal/dto/general_dto.go
+++ services/app-auth/internal/dto/general_dto.go
@@ -2,7 +2,7 @@ package dto
 
 import "github.com/AppsLab-KE/backend-everyshilling/services/app-authentication/internal/core/entity"
 
-type DefaultRes[T interface{}] struct {
+type DefaultRes[T UserRegistrationRes | UserLoginRes | ResetRes] struct {
 	Message string `json:"message"`
 	Error   string `json:"errors"`
 	Code    int    `json:"code"`
