082624daefc70eea07f57cc7bb1296def78f0434
MikeMwita <mwitamike8@gmail.com>
2023-04-13 22:15:12 +0300

    debugged signup handler

--- services/app-auth/internal/errors/error.go
+++ services/app-auth/internal/errors/error.go
@@ -3,7 +3,8 @@ package errors
 import "errors"
 
 var (
-	ErrUserExists      = errors.New("user exists")
-	ErrUserCreation    = errors.New("io error while creating user. see server logs")
-	ErrValidationError = errors.New("validation error")
+	ErrUserExists   = errors.New("user exists")
+	ErrUserCreation = errors.New("io error while creating user. see server logs")
+	//ErrValidationError   = errors.New("validation error")
+	ErrRequestValidation = errors.New("validation  error")
 )
