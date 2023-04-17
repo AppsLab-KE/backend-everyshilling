0c8c770d7ad7eb8ff0ec6c8dd2083e1045908f6b
Marvin Collins <marvin@Marvins-MacMini.Local>
2023-04-17 12:57:19 +0300

    WIP

--- docs/protos/generate-go-grpc.sh
+++ docs/protos/generate-go-grpc.sh
@@ -6,4 +6,3 @@ protoc --go_out=./../../sdk/go-proto-gen --go_opt=paths=source_relative --go-grp
 
 #Generate otp proto to sdk/go-proto-gen
 protoc --go_out=./../../sdk/go-proto-gen --go_opt=paths=source_relative --go-grpc_out=./../../sdk/go-proto-gen --go-grpc_opt=paths=source_relative otp/server.proto
-protoc --go_out=./../../sdk/go-proto-gen --go_opt=paths=source_relative --go-grpc_out=./../../sdk/go-proto-gen --go-grpc_opt=paths=source_relative otp/otp.proto
