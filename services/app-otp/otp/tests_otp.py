93e19c8ebc41d9c19452956403a698a513438c83
cosBett <cosmasbett9@gmail.com>
2023-05-14 17:34:03 +0300

    modify tests for otp generation

--- services/app-otp/otp/tests_otp.py
+++ services/app-otp/otp/tests_otp.py
@@ -1,46 +1,29 @@
 import os
 import unittest
-from unittest.mock import Mock, patch
-import otp
+from otp import generate_otp, send_otp
 
-class TestOTPFunctions(unittest.TestCase):
-    @patch ('otp.struct')
-    @patch ('otp.hmac')
-    def test_generate_otp(self, hmac_mock,struct_mock):
-        struct_mock.pack.return_value= b'test_bytes'
-        hmac_mock.new.return_value= b'test_hash'
-        struct_mock.upack.return_value= (123456)
-        
-        
-        generated_otp=otp.generate_otp()
-        
-        self.assertEqual(generated_otp,'234567')
-        
-        hmac_mock.new.assert_called_once_with(b"test_secret", b"test_bytes", otp.hashlib.sha1)
-        struct_mock.pack.assert_called_once_with(">Q", Mock())
-        struct_mock.unpack.assert_called_once_with(">I", b"test_hash"[11:15])
-        
-    @patch("otp.sms")
-    def test_send_otp_success(self, sms_mock):
-        
-        response = {"SMSMessageData": {"Recipients": [{"status": "Success"}]}}
-        sms_mock.send.return_value = response
-        
-        
-        result = otp.send_otp('+254701847888', '123456')
-        
+
+class TestOTP(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set up the environment variables
+        os.environ['OTP_SECRET'] = 'my_secret_key'
+        os.environ['AFR_USERNAME'] = 'my_africastalking_username'
+        os.environ['AFRICASTALKING_API_KEY'] = 'my_africastalking_api_key'
+
+    def test_generate_otp(self):
+        # Test that an OTP is generated successfully
+        otp = generate_otp()
+        self.assertIsInstance(otp, str)
+        self.assertEqual(len(otp), 6)
+
+    def test_send_otp(self):
+        # Test that an OTP is sent successfully
+        phone_number = '+254701847888'
+        otp = generate_otp()
+        result = send_otp(phone_number, otp)
         self.assertTrue(result)
-        sms_mock.send.assert_called_once_with('Your OTP is: 123456. It expires in 5 minutes. Please do not share it with anyone.', ['+254701847888'])       
-        
-    @patch("otp.os")
-    @patch("otp.sms")
-    def test_send_otp_failure(self, sms_mock, os_mock):
-        
-        response = {"SMSMessageData": {"Recipients": [{"status": "Failed"}]}}
-        sms_mock.send.return_value = response
-        
-        result = otp.send_otp("+254701847888", "123456")
-        
-        self.assertFalse(result)
-        sms_mock.send.assert_called_once_with('Your OTP is: 123456. It expires in 5 minutes. Please do not share it with anyone.', ['+254701847888'])
-        os_mock.write.assert_called_once_with(2, b'{"SMSMessageData": {"Recipients": [{"status": "Failed"}]}}\n')    
+
+
+if __name__ == '__main__':
+    unittest.main()
