c4bab1eeadf97aeecf74bb27a4ddf6e5fa211f43
cosBett <cosmasbett9@gmail.com>
2023-05-16 18:06:31 +0300

    updated unittest for cache

--- services/app-otp/otp/tests_cache.py
+++ services/app-otp/otp/tests_cache.py
@@ -1,14 +1,19 @@
 import os
 import unittest
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 import redis
 from unittest.mock import patch
 from cache import save_otp, get_otp, get_phone_number, delete_otp_phone, delete_otp_tracking
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_PORT = int(os.getenv("REDIS_PORT", '6379'))
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+
+def get_mock_time():
+    # Return a mock time 5 minutes ahead
+    return datetime.now() + timedelta(minutes=5)
 
 
 class TestOTP(unittest.TestCase):
@@ -33,7 +38,10 @@ class TestOTP(unittest.TestCase):
         otp = '123456'
         save_otp(phone_number, tracking_uuid, otp)
         result = get_otp(phone_number)
-        expected = {"otp": otp.encode(), "time_stamp_unix": str(datetime.now().timestamp()).encode()}
+        expected = {
+            b'otp': otp.encode(),
+            b'time_stamp_unix': str(round(datetime.now().timestamp(), 2)).encode()
+        }
         self.assertEqual(result, expected)
 
     def test_get_phone_number(self):
@@ -43,9 +51,9 @@ class TestOTP(unittest.TestCase):
         save_otp(phone_number, tracking_uuid, otp)
         result = get_phone_number(tracking_uuid)
         expected = {
-            "phone_number": phone_number.encode(),
-            "otp": otp.encode(),
-            "time_stamp_unix": str(datetime.now().timestamp()).encode(),
+            b"phone_number": phone_number.encode(),
+            b"otp": otp.encode(),
+            b"time_stamp_unix": str(round(datetime.now().timestamp(), 2)).encode(),
         }
         self.assertEqual(result, expected)
 
@@ -67,16 +75,25 @@ class TestOTP(unittest.TestCase):
         result = get_otp(tracking_uuid)
         self.assertFalse(result)
 
-    @patch.object(datetime, "now")
-    def test_otp_expiry(self, mock_now):
+    def test_otp_expiry(self):
         phone_number = '+254701847888'
         tracking_uuid = 'abc-dc123'
         otp = '123456'
+        save_time = datetime.now()
         save_otp(phone_number, tracking_uuid, otp)
-        mock_now.return_value = datetime.now() + timedelta(minutes=5)
 
-        result = get_otp(phone_number)
-        self.assertFalse(result)
+        with patch('cache.datetime') as mock_datetime:
+            mock_datetime.now.side_effect = lambda: get_mock_time(save_time)
+            result = get_otp(phone_number)
+
+        expected = {
+            b'otp': otp.encode(),
+            b'time_stamp_unix': str(round(get_mock_time().timestamp(), 2)).encode()
+        }
+        # Check if the OTP is expired
+        current_time = datetime.now().timestamp()
+        expiration_time = float(result.get(b'time_stamp_unix', 0))
+        self.assertTrue(current_time > expiration_time)
 
 
 if __name__ == '__main__':
